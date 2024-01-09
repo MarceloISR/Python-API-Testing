6542bff34e4ac99767841302debb60358f3d527f
Marcelo Sanabria <ing.marcelo.sanabria@gmail.com>
2024-01-09 11:53:48 -0400

    created config.py file

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+API_BASE_URL = os.environ["API_URL"]
+
+API_KEY =  os.environ["API_KEY"]
+API_SECRET = os.environ["API_SECRET"]
+
+# print("url:", API_BASE_URL)
+# print("key:", API_KEY)
+# print("secret:", API_SECRET)
