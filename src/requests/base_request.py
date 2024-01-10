47914f1c1ba2eb4163437117edd4446e946b7637
Marcelo Sanabria <ing.marcelo.sanabria@gmail.com>
2024-01-10 04:11:11 -0400

    added authorization tests

---
+++ src/requests/base_request.py
@@ -0,0 +1,31 @@
+from enum import Enum
+import requests
+
+from src.requests.api_endpoints import EndPoints
+from utils.config import API_BASE_URL, CONFIG_HEADERS
+
+class Methods(Enum):
+    GET="GET"
+    POST="POST"
+    PUT="PUT"
+    DELETE="DELETE"
+
+
+
+class Base_Request:
+
+    def set_headers(self, headers=None):
+        self._headers = headers or CONFIG_HEADERS
+
+    def set_request(self, url: EndPoints, method: Methods | None= None, headers=None, params=None):
+        self._url = f"{API_BASE_URL}/{url.value}"
+        if method is not None:
+            self._method=method.value
+        else:
+            self._method=method
+        self._headers = headers or CONFIG_HEADERS
+        self._params=params
+
+    def request(self,method: Methods,url: EndPoints,   headers=None, params=None):
+        self.set_request(url, method, headers, params)
+        return requests.request(url=self._url, method=self._method, params=self._params, headers=self._headers)
---
+++ tests/test_authorization.py
@@ -0,0 +1,49 @@
+
+import pytest
+
+from utils.config import API_KEY, API_SECRET
+
+from src.requests.api_endpoints import EndPoints
+from src.requests.base_request import Base_Request, Methods
+
+"""Fixture to share accross all the test scenarios a Base_Request object"""
+@pytest.fixture
+def Req():
+    return Base_Request()
+
+"""Scenario 1: Missing Authorization header """
+@pytest.mark.authorization
+@pytest.mark.search_endpoing
+def test_missing_authorization_secret(Req:Base_Request):
+    # Send a search request without the Authorization header.
+    _headers= {'Authorization': f"Discogs key={API_KEY}, secret=''"}
+    response = Req.request(Methods.GET, EndPoints.SEARCH, headers=_headers)
+
+    # Assert that the response status code is 401 Unauthorized.
+    assert response.status_code==401, f'the status code should be 401 instead of {response.status_code}'
+    # Assert that the response message indicates missing or invalid authorization."""
+    assert response.json()["message"] == "Invalid consumer key/secret. Please register an app before making requests."
+
+
+"""Scenario 2: Invalid Authorization header format """
+@pytest.mark.authorization
+@pytest.mark.search_endpoing
+def test_invalid_authorization_header_format(Req:Base_Request):
+    # Send a search request with an incorrectly formatted Authorization header (e.g., wrong format, missing Discogs text).
+    _headers= {'Authorization': f"key={API_KEY}, secret={API_SECRET}"}
+    response = Req.request(Methods.GET, EndPoints.SEARCH, headers=_headers)
+    # Assert that the response status code is 401 Unauthorized.
+    assert response.status_code==401, f'the status code should be 401 instead of {response.status_code}'
+    # Assert that the response message indicates invalid authorization."""
+    assert response.json()["message"] == "You must authenticate to access this resource."
+
+
+"""Scenario 3: Expired session """
+@pytest.mark.authorization
+@pytest.mark.search_endpoing
+@pytest.mark.skip(reason="Not implemented yet")
+def test_expired_session(Req: Base_Request):
+    # Send a search request with an expired session.
+    # Assert that the response status code is 401 Unauthorized.
+    # Assert that the response message indicates invalid authorization.
+    None
